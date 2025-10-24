d0b8d1899106311b0667b80ba8d6f20b77446cba
Skylar Baker <skylar@baker.network>
2025-10-23 22:20:48 -0700

    Remove groundhog clearing and custom file

--- Exopelago/Exopelago/GroundhogPatch.cs
+++ Exopelago/Exopelago/GroundhogPatch.cs
@@ -7,56 +7,56 @@ using Exopelago.Archipelago;
 
 namespace Exopelago;
 
-[HarmonyPatch(typeof(Groundhogs))]
-public static class GroundhogsPatch
-{
-	[HarmonyPatch("LoadInner")]
-	[HarmonyPatch("GetGroundhogContents")]
-	[HarmonyPatch("SaveThread")]
-	[HarmonyPrefix]
-	static void Prefix()
-	{
-		string documents = FileManager.documentsPath;
-		string path = Path.Combine(documents, ArchipelagoData.GroundhogsFileName);
-		if (!File.Exists(path)) {
-			using (StreamWriter sw = File.AppendText(path)) {
-				sw.WriteLine("{}");
-			}
-		}
-		path = Path.Combine(documents, ArchipelagoData.GroundhogsFileNameBackup);
-		if (!File.Exists(path)) {
-			using (StreamWriter sw = File.AppendText(path)) {
-				sw.WriteLine("{}");
-			}
-		}
-	}
-	[HarmonyPatch("LoadInner")]
-	[HarmonyPatch("GetGroundhogContents")]
-	[HarmonyPatch("SaveThread")]
-	[HarmonyTranspiler]
-	static IEnumerable<CodeInstruction> jsonTranspiler(IEnumerable<CodeInstruction> instructions)
-	{
-		var codeMatcher = new CodeMatcher(instructions);
-		codeMatcher.MatchForward(false,
-			new CodeMatch(OpCodes.Ldstr, "Groundhogs.json", "fileName")
-			)
-			.SetInstructionAndAdvance(
-				new CodeInstruction(OpCodes.Call, typeof(Exopelago.Archipelago.ArchipelagoData).GetMethod("get_GroundhogsFileName"))
-			);
-		return codeMatcher.Instructions();
-	}
-
-	[HarmonyPatch("LoadInner")]
-	[HarmonyTranspiler]
-	static IEnumerable<CodeInstruction> bakTranspiler(IEnumerable<CodeInstruction> instructions)
-	{
-		var codeMatcher = new CodeMatcher(instructions);
-		codeMatcher.MatchForward(false,
-			new CodeMatch(OpCodes.Ldstr, "Groundhogs.bak", "fileName")
-			)
-			.SetInstructionAndAdvance(
-				new CodeInstruction(OpCodes.Call, typeof(Exopelago.Archipelago.ArchipelagoData).GetMethod("get_GroundhogsFileNameBackup"))
-			);
-		return codeMatcher.Instructions();
-	}
-}
+//[HarmonyPatch(typeof(Groundhogs))]
+//public static class GroundhogsPatch
+//{
+//	[HarmonyPatch("LoadInner")]
+//	[HarmonyPatch("GetGroundhogContents")]
+//	[HarmonyPatch("SaveThread")]
+//	[HarmonyPrefix]
+//	static void Prefix()
+//	{
+//		string documents = FileManager.documentsPath;
+//		string path = Path.Combine(documents, ArchipelagoData.GroundhogsFileName);
+//		if (!File.Exists(path)) {
+//			using (StreamWriter sw = File.AppendText(path)) {
+//				sw.WriteLine("{}");
+//			}
+//		}
+//		path = Path.Combine(documents, ArchipelagoData.GroundhogsFileNameBackup);
+//		if (!File.Exists(path)) {
+//			using (StreamWriter sw = File.AppendText(path)) {
+//				sw.WriteLine("{}");
+//			}
+//		}
+//	}
+//	[HarmonyPatch("LoadInner")]
+//	[HarmonyPatch("GetGroundhogContents")]
+//	[HarmonyPatch("SaveThread")]
+//	[HarmonyTranspiler]
+//	static IEnumerable<CodeInstruction> jsonTranspiler(IEnumerable<CodeInstruction> instructions)
+//	{
+//		var codeMatcher = new CodeMatcher(instructions);
+//		codeMatcher.MatchForward(false,
+//			new CodeMatch(OpCodes.Ldstr, "Groundhogs.json", "fileName")
+//			)
+//			.SetInstructionAndAdvance(
+//				new CodeInstruction(OpCodes.Call, typeof(Exopelago.Archipelago.ArchipelagoData).GetMethod("get_GroundhogsFileName"))
+//			);
+//		return codeMatcher.Instructions();
+//	}
+//
+//	[HarmonyPatch("LoadInner")]
+//	[HarmonyTranspiler]
+//	static IEnumerable<CodeInstruction> bakTranspiler(IEnumerable<CodeInstruction> instructions)
+//	{
+//		var codeMatcher = new CodeMatcher(instructions);
+//		codeMatcher.MatchForward(false,
+//			new CodeMatch(OpCodes.Ldstr, "Groundhogs.bak", "fileName")
+//			)
+//			.SetInstructionAndAdvance(
+//				new CodeInstruction(OpCodes.Call, typeof(Exopelago.Archipelago.ArchipelagoData).GetMethod("get_GroundhogsFileNameBackup"))
+//			);
+//		return codeMatcher.Instructions();
+//	}
+//}
